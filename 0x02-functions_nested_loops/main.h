eaa87a320f57924097c53c78bcc159586a7e1c68
linkerAlx <linker222006766@gmail.com>
2023-09-12 09:33:20 -0700

    qnswer 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef main_h
 #define main_h
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,12 +13,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-
-
-
-
-
-
 
 #endif
